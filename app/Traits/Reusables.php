8ea7fb78c1dd92a2453b11946f2f4036389ceac8
Adeiza <alexmercyadeiza@gmail.com>
2024-05-22 00:37:34 +0100

    [fix] test ipinfo in prod #2

--- app/Traits/Reusables.php
+++ app/Traits/Reusables.php
@@ -3,6 +3,8 @@
 namespace App\Traits;
 
 use GuzzleHttp\Client;
+use Jenssegers\Agent\Agent;
+use Stevebauman\Location\Facades\Location;
 
 trait Reusables
 {
@@ -21,5 +23,34 @@ trait Reusables
                 'content' => $message
             ]
         ]);
+    }
+
+    public function getUserDetails()
+    {
+        $agent = new Agent();
+        $ip = request()->ip();
+        $location = Location::get($ip);
+
+        $browser = $agent->browser();
+        $browserVersion = $agent->version($browser);
+        $platform = $agent->platform();
+        $platformVersion = $agent->version($platform);
+
+        return [
+            'ip_address' => $ip,
+            'browser' => $browser,
+            'browser_version' => $browserVersion,
+            'platform' => $platform,
+            'platform_version' => $platformVersion,
+            'location' => $location ? [
+                'country' => $location->countryName,
+                'region' => $location->regionName,
+                'city' => $location->cityName,
+                'zip_code' => $location->zipCode,
+                'latitude' => $location->latitude,
+                'longitude' => $location->longitude,
+                'timezone' => $location->timezone,
+            ] : null,
+        ];
     }
 }
