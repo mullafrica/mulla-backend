c661f188b976ed3f6e2e66bff79bdfd1bfcdfdec
Adeiza <alexmercyadeiza@gmail.com>
2024-05-22 22:22:10 +0100

    [fix] email test #4

--- resources/views/mail/mulla-reset-token-email.blade.php
+++ resources/views/mail/mulla-reset-token-email.blade.php
@@ -7,13 +7,13 @@
 <body style="background-color: #eeeeee; margin: 0; padding: 0;">
     <div style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; padding: 0; background-color: #eeeeee;">
         <div style="padding: 2.5rem; max-width: 600px; margin: 40px auto; background-color: #ffffff; box-sizing: border-box; border-radius: 0 0 0.75rem 0.75rem;">
-            <header>
+            <header style="border-bottom: 0.05px solid #797979; padding-bottom: 2.5rem;">
                 <div>
                     <img src="https://awstestbucket-pk.s3.eu-west-1.amazonaws.com/logo-black.png" alt="Mulla Africa Logo" style="width: 130px;" />
                 </div>
             </header>
 
-            <div style="margin-top: 50px; background-color: #fff; box-sizing: border-box; border-radius: 0 0 0.75rem 0.75rem;">
+            <div style="margin-top: 40px; background-color: #fff; box-sizing: border-box; border-radius: 0 0 0.75rem 0.75rem;">
                 <div style="padding-bottom: 2rem; font-size: 15px;">{{ $firstname ?? '' }},</div>
                 <div style="padding-bottom: 2rem; font-size: 15px;">Enter this 6-digit code to verify it's really you on the other end of this.</div>
                 <div style="font-size: 30px; padding-bottom: 2rem;">{{ $token ?? '' }}</div>
