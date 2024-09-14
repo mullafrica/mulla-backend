7fed8fe7eb57df56d8ebca92b68f4f3382feb87f
Alex Mercy Adeiza <alexmercyadeiza@gmail.com>
2024-09-14 05:46:08 +0100

    [fix] insignts

--- app/Http/Controllers/MullaStatsController.php
+++ app/Http/Controllers/MullaStatsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\MullaUserTransactions;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
@@ -11,17 +12,18 @@ class MullaStatsController extends Controller
 {
     public function getStats()
     {
-        $id = Auth::id();
+        $user = Auth::user();
 
-        $data = Cache::remember('user_stats_cache' . $id, 60 * 24 * 24, function () use ($id) {
+        $data = Cache::remember('user_stats_cache' . $user->id, 60 * 24 * 24, function () use ($user) {
             return [
-                'total_amount_spent' => number_format(MullaUserTransactions::where('user_id', $id)->where('status', 1)->whereNot('type', 'Bank Transfer')->sum('amount'), 2),
-                'total_transaction_count' => MullaUserTransactions::where('user_id', $id)->where('status', 1)->count(),
+                'created_at' => Carbon::parse($user->created_at)->isoFormat('LL'),
+                'total_amount_spent' => number_format(MullaUserTransactions::where('user_id', $user->id)->where('status', 1)->whereNot('type', 'Bank Transfer')->sum('amount'), 2),
+                'total_transaction_count' => MullaUserTransactions::where('user_id', $user->id)->where('status', 1)->count(),
                 'spend_by_type' => [
-                    'electricity' => number_format(MullaUserTransactions::where('user_id', $id)->where('status', 1)->where('type', 'Electricity Bill')->sum('amount'), 2),
-                    'airtime' => number_format(MullaUserTransactions::where('user_id', $id)->where('status', 1)->where('type', 'Airtime Recharge')->sum('amount'), 2),
-                    'tv' => number_format(MullaUserTransactions::where('user_id', $id)->where('status', 1)->where('type', 'TV Subscription')->sum('amount'), 2),
-                    'internet_data' => number_format(MullaUserTransactions::where('user_id', $id)->where('status', 1)->where('type', 'Data Services')->sum('amount'), 2),
+                    'electricity' => number_format(MullaUserTransactions::where('user_id', $user->id)->where('status', 1)->where('type', 'Electricity Bill')->sum('amount'), 2),
+                    'airtime' => number_format(MullaUserTransactions::where('user_id', $user->id)->where('status', 1)->where('type', 'Airtime Recharge')->sum('amount'), 2),
+                    'tv' => number_format(MullaUserTransactions::where('user_id', $user->id)->where('status', 1)->where('type', 'TV Subscription')->sum('amount'), 2),
+                    'internet_data' => number_format(MullaUserTransactions::where('user_id', $user->id)->where('status', 1)->where('type', 'Data Services')->sum('amount'), 2),
                 ]
             ];
         });
