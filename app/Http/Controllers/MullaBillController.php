ee13517ea4d63bc843ee2e37766499215c0e50c8
Alex Mercy Adeiza <alexmercyadeiza@gmail.com>
2024-11-02 15:47:45 +0100

    [fix] bill updates, also preg fix entry to sanitize it, remove cache from get bill operators

--- app/Http/Controllers/MullaBillController.php
+++ app/Http/Controllers/MullaBillController.php
@@ -223,26 +223,25 @@ class MullaBillController extends Controller
         }
 
         if ($identifier) {
-            $response = Cache::remember('operator_products_' . $identifier, 60 * 60 * 24, function () use ($identifier) {
-                $ops = Http::withHeaders([
-                    'api-key' => env('VTPASS_API_KEY'),
-                    'public-key' => env('VTPASS_PUB_KEY')
-                ])->get($this->vtp_endpoint() . 'services?identifier=' . $identifier);
+            // $response = Cache::remember('operator_products_' . $identifier, 60 * 60 * 24, function () use ($identifier) {
+            $ops = Http::withHeaders([
+                'api-key' => env('VTPASS_API_KEY'),
+                'public-key' => env('VTPASS_PUB_KEY')
+            ])->get($this->vtp_endpoint() . 'services?identifier=' . $identifier);
 
-                $data = $ops->json();
+            $data = $ops->json();
 
-                $mappedContent = array_map(function ($service) {
-                    $service['id'] = $service['serviceID']; // Create a new 'id' property
-                    unset($service['serviceID']); // Remove the original 'serviceID' property
-                    return $service;
-                }, $data['content']);
+            $mappedContent = array_map(function ($service) {
+                $service['id'] = $service['serviceID']; // Create a new 'id' property
+                unset($service['serviceID']); // Remove the original 'serviceID' property
+                return $service;
+            }, $data['content']);
 
-                $response['data'] = $mappedContent;
-
-                return $response;
-            });
+            $response['data'] = $mappedContent;
 
             return $response;
+            // });
+            // return $response;
         } else {
             return response()->json(['error' => 'Bill identifier not provided'], 400);
         }
@@ -367,7 +366,7 @@ class MullaBillController extends Controller
         ]);
 
         $amount = (float) preg_replace('/[^0-9.]/', '', $request->amount);
-        
+
         if ($amount <= 0) {
             return response()->json(['message' => 'Amount cannot be zero.'], 400);
         }
