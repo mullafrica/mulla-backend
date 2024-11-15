4309925472425f1f8fce279bae96593614baafae
Alex Mercy Adeiza <alexmercyadeiza@gmail.com>
2024-11-15 18:39:50 +0100

    [fix] add a few more fields

--- app/Services/PushNotification.php
+++ app/Services/PushNotification.php
@@ -9,14 +9,20 @@ class PushNotification
 {
     public function send($data)
     {
-        // if (is_array($data)) {
-        //     $data = array_map(function ($item) {
-        //         $item['badge'] = 1;
-        //         $item['channelId'] = 'default';
-        //         $item['sound'] = 'default';
-        //         return $item;
-        //     }, $data);
-        // }
+        if (is_array($data)) {
+            if (isset($data[0]) && is_array($data[0])) {
+                $data = array_map(function ($item) {
+                    $item['badge'] = 1;
+                    $item['channelId'] = 'default';
+                    $item['sound'] = 'default';
+                    return $item;
+                }, $data);
+            } else {
+                $data['badge'] = 1;
+                $data['channelId'] = 'default';
+                $data['sound'] = 'default';
+            }
+        }
 
         PushNotificationJob::dispatch($data);
     }
