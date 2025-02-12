ccf5696292707562aa7227dc9b0d99d9c3265dbd
Alex Mercy Adeiza <alexmercyadeiza@gmail.com>
2025-02-12 13:31:32 +0100

    [fix] user status guards

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,6 +24,17 @@ class Kernel extends HttpKernel
     ];
 
     /**
+     * The application's route middleware.
+     *
+     * These middleware may be assigned to groups or used individually.
+     *
+     * @var array<string, class-string|string>
+     */
+    protected $routeMiddleware = [
+        'checkUserStatus' => \App\Http\Middleware\CheckUserStatus::class,
+    ];
+
+    /**
      * The application's route middleware groups.
      *
      * @var array<string, array<int, class-string|string>>
---
+++ app/Http/Middleware/CheckUserStatus.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckUserStatus
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+
+        // If the user exists but the status is false, block access.
+        if ($user && !$user->status) {
+            return response()->json(['message' => 'Account inactive'], 400);
+        }
+
+        return $next($request);
+    }
+}
