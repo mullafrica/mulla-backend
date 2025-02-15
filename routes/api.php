cbf6a7eb88c81962e11f5d3e819e849b33f3785f
mullafrica <mulla.africa@gmail.com>
2025-02-15 10:46:43 +0000

    reg

--- routes/api.php
+++ routes/api.php
@@ -34,14 +34,14 @@ use Illuminate\Support\Facades\Route;
 Route::post('/comet/auth', [MullaAuthController::class, 'login']);
 
 Route::post('/comet/auth/register/web', [MullaAuthController::class, 'register']);
-// Route::post('/comet/auth/verify/web', [MullaAuthController::class, 'registrationToken']);
+Route::post('/comet/auth/verify/web', [MullaAuthController::class, 'registrationToken']);
 
 // Route::post('/comet/auth/verify', [MullaAuthController::class, 'registrationToken']);
 Route::post('/comet/auth/register', [MullaAuthController::class, 'register']);
 
-Route::post('/comet/auth/verify/web', function (Request $request) {
-    return response(['message' => 'New account registration temporarily suspended, please check back later.'], 400);
-});
+// Route::post('/comet/auth/verify/web', function (Request $request) {
+   // return response(['message' => 'New account registration temporarily suspended, please check back later.'], 400);
+// ));
 
 Route::post('/comet/auth/verify', function (Request $request) {
     return response(['message' => 'New account registration temporarily suspended, please check back later.'], 400);
